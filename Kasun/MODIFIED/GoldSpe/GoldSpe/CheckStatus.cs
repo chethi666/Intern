a334f50a8e739c112b868e9d8c2b8eac8f367a6c
Kasun Guruge <kasunrajitha733@gmail.com>
2017-12-04 16:49:14 +0530

    GoldSpe in MODIFIED folder(Service app)

---
+++ Kasun/MODIFIED/GoldSpe/GoldSpe/CheckStatus.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSpe
+{
+    class CheckStatus
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public void Check()
+        {
+           
+           
+            RetrieveRobots rt = new RetrieveRobots();
+            
+            int index = 0;
+
+
+            List<string> AvailableRobots = rt.GetRobots();
+
+
+            foreach (string line in AvailableRobots)
+            {
+               
+                index++;
+                var Today = DateTime.Now;
+                var Yesterday = DateTime.Now.AddDays(-1);
+                string x = Today.ToString("yyyy-MM-dd");
+                string y = Yesterday.ToString("yyyy-MM-dd");
+               
+
+                try
+                {
+                    string v = string.Format("http://checkmate.ukwest.cloudapp.azure.com:9100/documentServer/documentRepository/documents?query=SourceUrl:" + "\"" + "{0}" + "\"" + " AND PublishDate:" + "[" + y + " TO " + x + "]&sortByField=PublishDate&sortDescending=true", line.Trim());  //Adding Date
+
+
+                    var response2 = new WebClient().DownloadString(v);
+
+
+
+
+                    JObject jObj1 = JObject.Parse(response2);
+
+                    if (jObj1["Documents"] != null && jObj1["Documents"].HasValues == true)
+                    {
+                        string id = (string)jObj1["Documents"][0]["_id"];
+
+                        if (response2 != null && string.IsNullOrEmpty(id) == false)
+                        {
+
+
+
+                            logger.Info("The url of Robot= " + line + "=====is  working\n");
+                        }
+                        else
+                        {
+                       
+                            logger.Error("The url of Robot= " + line + "=====is not working due to no Robot_ID values \n");
+                        }
+                    }
+                    else
+                    {
+
+                  
+                        logger.Error("The url of Robot= " + line + "=====is not working due to no Document values \n");
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e);
+                }
+
+
+            }
+
+
+        }
+
+        
+
+    }
+}
---
+++ Kasun/MODIFIED/GoldSpe/GoldSpe/MyService.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSpe
+{
+    public class MyService
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public void Start()
+        {
+            
+            CheckStatus check = new CheckStatus();
+            check.Check();
+        }
+        public void Stop()
+        {
+            
+            logger.Error("Service is Stopped");
+        }
+    }
+}
