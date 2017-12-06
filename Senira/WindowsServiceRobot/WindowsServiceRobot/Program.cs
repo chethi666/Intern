2e5b53aaa3e259ad73df4ac9745b077ba56dd715
senirakeshan <senirakeshan@s2t.com.sg>
2017-12-06 13:46:30 +0530

    windows service application for robot checking

--- Senira/WindowsServiceRobot/WindowsServiceRobot/Program.cs
+++ Senira/WindowsServiceRobot/WindowsServiceRobot/Program.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Net;
-using Newtonsoft.Json.Linq;
-using NLog;
-using Topshelf;
+
 
 namespace WindowsServiceRobot
 {
@@ -19,11 +15,9 @@ namespace WindowsServiceRobot
         {
             Console.Out.WriteLine("Greetings, some loggings is about to take minutes.");
             Console.Out.WriteLine("");
-            // ConfigureService.Configure();
+         ConfigureService.Configure();
 
-            Service1 myService = new Service1();
-                    myService.OnDebug();
-                   System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+               
 
         }
 
